de3940ac945d3cbefe7f0d97f4060192d28af64c
thetA Code <hermes.ralph@hotmail.com>
2012-03-31 17:54:02 +0200

    WebToolz_#1

--- WebToolz_#1/index.php
+++ WebToolz_#1/index.php
@@ -2,11 +2,17 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link rel="stylesheet" href="./css/style.css" type="text/css">
+        <script src="./js/functions.js"></script>
         <title></title>
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+        <div id="button">
+            <div id="highlight">
+                <div id="overlay">
+                    <label id="btntext" onClick="setPos(40)">Button</label>
+                </div>
+            </div>
+        </div>
     </body>
 </html>
